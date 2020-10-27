63937c8e668eab9a99f2393d1206faa3c3d87081
HASNAIN UD DIN BILAL <hasnain@quinin.com>
2020-10-27 23:12:20 +0500

    fixed version

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 const Card = (props) =>{
     return(
@@ -9,7 +9,7 @@ const Card = (props) =>{
                     <div className="card-body">
                         <h5 className="card-title">{props.title}</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <button className="btn btn-primary">Go somewhere</button>
                     </div>
                 </div>
             </div>
